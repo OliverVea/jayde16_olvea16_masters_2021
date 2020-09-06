2d3f1b824419aa6b10ee20e70aee283c6e6320cb
Olvea16 <olvea16@student.sdu.dk>
2020-09-06 12:50:51 +0200

    small changes and examples

--- v0/main.py
+++ v0/main.py
@@ -1,12 +1,16 @@
 from wfs import WMTS, WFS, WFS_Feature
 from get_gps import GPSConnection
 from maps import MPL_Map
-from utility import printe
+from utility import printe, prints
 
 use_real_coords = False
 
 typenames = ['Mast', 'Nedloebsrist', 'Skorsten', 'Telemast', 'Trae', 'Broenddaeksel']
 colors = ['#FF0000', '#0000FF', '#FFFF00', '#FF00FF', '#00FF00', 'grey']
+
+gps_ip = '10.147.18.175'
+
+timeout = 2 # seconds
 
 wfs = WFS('https://services.datafordeler.dk/GeoDanmarkVektor/GeoDanmark60_NOHIST_GML3/1.0.0/WFS?', 
     username='VCSWRCSUKZ',
@@ -20,8 +24,9 @@ wmts = WMTS('https://services.datafordeler.dk/GeoDanmarkOrto/orto_foraar_wmts/1.
     tile_matrix_set='KortforsyningTilingDK')
 
 try:
-    conn = GPSConnection(address='10.147.18.175', port=7789)
-    coords = conn.get_coords(timeout=2).to_srs('EPSG:3857')
+    conn = GPSConnection(address=gps_ip, port=7789)
+    coords = conn.get_coords(timeout=timeout).to_srs('EPSG:3857')
+    prints(f'Got authentic coordinates from gps.')
 except:
     printe(f'Could not get coordinates. Using default instead.')
     coords = WFS_Feature(tag='GPS', geometry=(55.369837, 10.431700), default_srs='EPSG:4326').to_srs('EPSG:3857')
