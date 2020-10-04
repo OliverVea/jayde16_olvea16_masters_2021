fff0e5db22f2548db5dfabc6820916eabdd2bdd7
Olvea16 <olvea16@student.sdu.dk>
2020-10-04 14:29:54 +0200

    sdsa

--- v1/jaolma/properties.py
+++ v1/jaolma/properties.py
@@ -3,10 +3,10 @@ from jaolma.gis.wfs import Feature
 class Properties:
     areas = {
         'downtown': (55.3947509, 10.3833619), 
-        'suburb': (55.3761308, 10.3860752), 
-        'university_parking': (55.3685818, 10.4317584), 
         'harbor': (55.4083756, 10.3787729), 
-        'park': (55.3916561, 10.3828329)
+        'park': (55.3916561, 10.3828329),
+        'suburb': (55.3761308, 10.3860752), 
+        'university_parking': (55.3685818, 10.4317584)
     }
 
-    areas = {Feature(geometry, 'EPSG:4326', key) for key, geometry in zip(areas.keys(), areas.values())}
+    areas = {key: Feature(geometry, 'EPSG:4326', key) for key, geometry in zip(areas.keys(), areas.values())}
--- v1/plot_map.py
+++ v1/plot_map.py
@@ -1,6 +1,54 @@
+from jaolma.properties import Properties
+
 import sys
+import os
 
-file_path = sys.argv[1]
+try:
+    file_path = sys.argv[1]
+except:
+    path = os.path.join(os.getcwd(), 'files/areas/')
 
+    dirs = [f for f in os.listdir(path) if os.path.isdir(os.path.join(path, f)) and f in Properties.areas]
+
+    while True:
+        print('\n'.join([f'{i + 1}: {dir}' for i, dir in enumerate(dirs)]))
+        i = input('Please select an option or enter the full path of a .csv file: ')
+
+        if i.isnumeric():
+            i = int(i) - 1
+            if 0 > i or i >= len(dirs):
+                print('Invalid index.')
+                continue
+
+            dir = dirs[i]
+            break
+
+        else:
+            print(f'Using path \'{i}\'.')
+            file_path = i
+            break
+        
+    if isinstance(i, int):
+        path = os.path.join(path, dir)
+
+        files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f)) and f.endswith('.csv')]
+
+        while True:
+            print('\n'.join([f'{i + 1}: {dir}' for i, dir in enumerate(files)]))
+            i = input('Please select an option: ')
+
+            if i.isnumeric():
+                i = int(i) - 1
+                if 0 > i or i >= len(dirs):
+                    print('Invalid index.')
+                    continue
+
+                file = files[i]
+                break
+
+        file_path = path = os.path.join(path, file)
+
+with open(file_path, 'r') as f:
+    print(''.join([row for row in f]))
 
 input()
