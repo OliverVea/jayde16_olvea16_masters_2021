03fa1c3383e8686de562b10580aba38c2ff09958
JakobYde <44023323+JakobYde@users.noreply.github.com>
2020-10-04 14:31:11 +0200

    Update read_samaqua.py

--- v1/read_samaqua.py
+++ v1/read_samaqua.py
@@ -1,12 +1,47 @@
 from jaolma.utility.csv import *
+from jaolma.properties import Properties
+from math import sqrt
 
-def filter(row):
-    return row['X_Cover'] != 'NULL'
+radius = 100
 
-csv = CSV('input/Node_Cover_wHeader.csv', delimiter=',', type_row = True)
-a = csv.read(filter=filter)
+for area in Properties.areas:
 
-out = CSV('input/_Node_Cover_wHeader.csv', delimiter=',', type_row = True)
-out.write(a)
+    center = area.as_srs(srs='EPSG:25832').points['EPSG:25832']
 
-pass
+    def filter(row):
+        if row['X_Node'] != None:
+            if sqrt((row['X_Node'] - center[0])**2 + (row['Y_Node'] - center[1])**2) < radius:
+                return True
+
+        if row['X_Cover'] != None:
+            return sqrt((row['X_Cover'] - center[0])**2 + (row['Y_Cover'] - center[1])**2) < radius
+
+        return False
+        
+
+    input_csv = CSV('input/Node_Cover_wHeader.csv', delimiter=',', type_row = True)
+    input_data = input_csv.read(filter=filter)
+
+    output_data = [{}]*len(input_data)
+
+    cover_number = len(input_data)
+    for i, row in enumerate(input_data):
+        if row['X_Cover'] == None:
+            output_data[i]['#'] = i
+            output_data[i]['ID'] = row['FeatureGUID'] + '_node'
+            output_data[i]['Description'] = 'Water node'
+            output_data[i]['Geometry'] = f'{row["X_Node"]}, {row["Y_Node"]}'
+        else:
+            output_data.append({})
+            output_data[cover_number]['#'] = cover_number
+            output_data[cover_number]['ID'] = row['FeatureGUID'] + '_cover'
+            output_data[cover_number]['Description'] = 'Water cover'
+            output_data[cover_number]['Geometry'] = f'{row["X_Cover"]}, {row["Y_Cover"]}'
+            cover_number += 1
+
+
+
+    #out = CSV('input/_Node_Cover_wHeader.csv', delimiter=',', type_row = True)
+    #out.write(a)
+    dummy = 0
+    pass
