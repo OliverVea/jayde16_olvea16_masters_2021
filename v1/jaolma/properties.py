96a1f4621de48e780d6207feaa0924110346af15
JakobYde <44023323+JakobYde@users.noreply.github.com>
2020-10-25 17:42:45 +0100

    Update properties.py

--- v1/jaolma/properties.py
+++ v1/jaolma/properties.py
@@ -16,29 +16,8 @@ class Properties:
     radius = 100
 
     feature_properties = {
-<<<<<<< Updated upstream
-        'TL695099': {'label': 'Buildings (ko)', 'color': '#700000'},
-        'TL965167': {'label': 'Road Wells (ko)', 'color': '#ff00ff'},
-        'L418883_421469': {'label': 'Park Trees (ko)', 'color': '#00851b'},
-        'L167365_421559': {'label': 'Park Points (ko)', 'color': '#6e452f'},
-        'Bygning': {'label': 'Building (gd)', 'color': '#ff0000'},
-        'Broenddaeksel': {'label': 'Manhole Cover (gd)', 'color': '#ac0fdb'},
-        'Mast': {'label': 'Light Fixture (gd)', 'color': '#e0de38'},
-        'Hegn': {'label': 'Fence (gd)', 'color': '#cf8b5b'},
-        'Soe': {'label': 'Lake (gd)', 'color': '#3051c7'},
-        'KratBevoksning': {'label': 'Bushes (gd)', 'color': '#3b6e2f'},
-        'Trae': {'label': 'Tree (gd)', 'color': '#30c74e'},
-        'Nedloebsrist': {'label': 'Downspout Grille (gd)', 'color': '#db0f64'},
-        'Chikane': {'label': 'Chicane (gd)', 'color': '#8db09b'},
-        'Vandloebskant': {'label': 'Stream (gd)', 'color': '#6e86db'},
-        'Helle': {'label': 'Refuge Island (gd)', 'color': '#3f4f46'},
-        'Skorsten': {'label': 'Chimney (gd)', 'color': '#292927'},
-        'Jernbane': {'label': 'Railroad (gd)', 'color': '#75756a'},
-        'Bassin': {'label': 'Pool (gd)', 'color': '#00a2ff'},
-        'water_node': {'label': 'Water Node (sa)', 'color': '#5b45a3'},
-=======
-        #'TL695099': {'origin': 'kortopslag', 'label': 'Buildings (ki)', 'color': '#700000'},
-        #'TL965167': {'origin': 'kortopslag', 'label': 'Road Wells (ki)', 'color': '#ff00ff'},
+        'TL695099': {'origin': 'kortopslag', 'label': 'Buildings (ki)', 'color': '#700000'},
+        'TL965167': {'origin': 'kortopslag', 'label': 'Road Wells (ki)', 'color': '#ff00ff'},
         'L418883_421469': {'origin': 'kortopslag', 'label': 'Park Trees (ki)', 'color': '#00851b'},
         'L167365_421559': {'origin': 'kortopslag', 'label': 'Park Points (ki)', 'color': '#6e452f'},
         'Bygning': {'origin': 'geodanmark', 'label': 'Building (gd)', 'color': '#ff0000'},
@@ -66,7 +45,6 @@ class Properties:
             'energifyn': {'url': 'https://services.drift.kortinfo.net/kortinfo/services/Wfs.ashx?Site=Odense&Page=Lyssignalanlaeg', 'version': '1.0.0'},
             'kortopslag': {'url': 'https://services.drift.kortinfo.net/kortinfo/services/Wfs.ashx?Site=Odense&Page=Kortopslag', 'version': '1.0.0'},
         }
->>>>>>> Stashed changes
     }
 
     @staticmethod
