d2de44af5dcbaeacdefd8034c8097021675dc077
Olvea16 <olvea16@student.sdu.dk>
2021-02-24 15:50:47 +0100

    dsadsa

--- camera_calibration/batch_transform_images.py
+++ camera_calibration/batch_transform_images.py
@@ -4,21 +4,45 @@ import cv2
 import numpy as np
 import os
 
-
-retval, K, d, rvecs, tvecs, stdDeviationsIntrinsics, stdDeviationsExtrinsics, perViewErrors, obj_points, img_points, not_used = np.load('camera_calibration_14dp_final.npy', allow_pickle=True)
+retval, K, d, rvecs, tvecs, stdDeviationsIntrinsics, stdDeviationsExtrinsics, perViewErrors, obj_points, img_points, not_used = np.load('camera_calibration_14dp_final_large.npy', allow_pickle=True)
 
 cam = Camera(camera_matrix=K, distortion_coefficients=d)
 
-image_size = (700, 500)
-image_fov = (150, 120)
+image_size = (640*2, 480*2)
+image_fov = (100, 120)
 
-path = 'angleimages/'
+show_image = True
+
+path = 'D:\\WindowsFolders\\Desktop\\camera_capture\\videos\\'
 
 files = os.listdir(path)
 
-for file in files:
-    img = cv2.imread(path + file)
-    img = cam.transform_image(img, image_size, image_fov)
+print(f'Found {len(files)} files:')
+print('\n'.join(files))
 
-    cv2.imshow("undistorted", img)
-    cv2.waitKey()
+fourcc = cv2.VideoWriter_fourcc(*'HFYU')
+vw = cv2.VideoWriter()
+
+for i, file in enumerate(files):
+    print(f'Undistorting file: {file}')
+
+    vc = cv2.VideoCapture(path + file)
+
+    vw.open(f'{file}.avi', fourcc, 21, image_size)
+
+    while True:
+        success, img = vc.read()
+
+        if not success or cv2.waitKey(10) == 27:
+            break
+
+        img_out = cam.transform_image(img, image_size, image_fov)
+
+        if show_image != False and (show_image == True or (isinstance(show_image, list) and show_image[i])): 
+            cv2.imshow('undistorted', img_out) 
+
+        vw.write(img_out)
+
+    vw.release()
+    
+    
--- camera_calibration/camera.py
+++ camera_calibration/camera.py
@@ -3,10 +3,27 @@ import numpy as np
 
 from math import sin, cos, pi
 
+from enum import Enum
+
+class CameraModels(Enum):
+    DEFAULT = 0
+    FISHEYE = 1
+
 class Camera:
-    def __init__(self, camera_matrix, distortion_coefficients):
+    def __init__(self, camera_matrix, distortion_coefficients, camera_model: str = None):
         self.K = camera_matrix
         self.d = distortion_coefficients
+        self.type = camera_model
+
+        if camera_model == CameraModels.FISHEYE:
+            self.projectPoints = cv2.fisheye.projectPoints
+            self.getOptimalNewCameraMatrix = cv2.fisheye.estimateNewCameraMatrixForUndistortRectify
+            self.initUndistortRectifyMap = cv2.fisheye.initUndistortRectifyMap
+
+        else:
+            self.projectPoints = cv2.projectPoints
+            self.getOptimalNewCameraMatrix = cv2.getOptimalNewCameraMatrix
+            self.initUndistortRectifyMap = cv2.initUndistortRectifyMap
     
     def transform_image(self, input, size: tuple, fov: tuple):
         input_size = (input.shape[1], input.shape[0])
@@ -23,7 +40,7 @@ class Camera:
             (0, sin(va), cos(va))])
 
         # Edge (center-)points in pixel coordinates
-        pts, _ = cv2.projectPoints(pts, rvec=np.zeros((3,1)), tvec=np.zeros((3,1)), cameraMatrix=self.K, distCoeffs=self.d)
+        pts, _ = self.projectPoints(pts, rvec=np.zeros((3,1)), tvec=np.zeros((3,1)), cameraMatrix=self.K, distCoeffs=self.d)
 
         # Calculating the necessary upscaling to crop the ROI to the desired image size.
         width = max(pts[:,0,0]) - min(pts[:,0,0])
@@ -33,10 +50,10 @@ class Camera:
         uncropped_size = (int(scale[0] * input_size[0]), int(scale[1] * input_size[1]))
 
         # Calculating new camera matrix (nK)
-        nK, _ = cv2.getOptimalNewCameraMatrix(cameraMatrix=self.K, distCoeffs=self.d, imageSize=input_size, newImgSize=uncropped_size, alpha=0.0)
+        nK, _ = self.getOptimalNewCameraMatrix(cameraMatrix=self.K, distCoeffs=self.d, imageSize=input_size, newImgSize=uncropped_size, alpha=0.0)
 
         # Undistoring image
-        map1, map2 = cv2.initUndistortRectifyMap(cameraMatrix=self.K, distCoeffs=self.d, R=np.eye(3,3), newCameraMatrix=nK, size=uncropped_size, m1type=cv2.CV_32FC1)
+        map1, map2 = self.initUndistortRectifyMap(cameraMatrix=self.K, distCoeffs=self.d, R=np.eye(3,3), newCameraMatrix=nK, size=uncropped_size, m1type=cv2.CV_32FC1)
         output = cv2.remap(input, map1, map2, interpolation=cv2.INTER_CUBIC)
         
         # Extracting principal point (center of camera)
